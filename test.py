d5d1628c203c658e1eafcb42ebcf11eb80574750
Pankti <pankti@dxfactor.com>
2023-12-12 11:34:24 +0530

    initial_commit

---
+++ test.py
@@ -0,0 +1,11 @@
+def multiply(a,b):
+    """
+    The function "multiply" takes two numbers as input and returns their product.
+    
+    :param a: The first parameter, `a`, represents the first number to be multiplied
+    :param b: The parameter "b" is the second number that will be multiplied with the first number "a"
+    :return: the product of the two input numbers, a and b.
+    """
+    return a*b
+
+print(multiply(5*6))
